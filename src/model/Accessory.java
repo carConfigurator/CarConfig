fe1c5a71d31a65000c112b852e189558d56d462e
LexFTW <lexmengual@gmail.com>
2018-10-19 20:10:34 +0200

    Models&Images

---
+++ src/model/Accessory.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Accessory {
+	
+	// Atributos de la Clase:
+	private int id;
+	private double price;
+	private String name, description, image_name;
+	private int[] models_available;
+
+}
