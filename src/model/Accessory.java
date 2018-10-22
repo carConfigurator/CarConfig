01e9389e32a690e38108d6860585e2b34fd8f407
LexFTW <lexmengual@gmail.com>
2018-10-22 16:22:33 +0200

    ModelsLoads

--- src/model/Accessory.java
+++ src/model/Accessory.java
@@ -7,5 +7,38 @@ public class Accessory {
 	private double price;
 	private String name, description, image_name;
 	private int[] models_available;
+	
+	public Accessory() {
+		this.id = loadId();
+		this.price = loadPrice();
+		this.name = loadName();
+		this.description = loadDescription();
+		this.image_name = loadImage_Name();
+		this.models_available = loadModels_Available();
+	}
+	
+	private int[] loadModels_Available() {
+		return null;
+	}
+
+	private String loadImage_Name() {
+		return null;
+	}
+
+	private String loadDescription() {
+		return null;
+	}
+
+	private String loadName() {
+		return null;
+	}
+
+	private double loadPrice() {
+		return 0;
+	}
+
+	private int loadId() {
+		return 0;
+	}
 
 }
