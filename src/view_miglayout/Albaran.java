d0ebaffac07e79bf36019195734eaffe47439a77
carConfigurator <carConfigurator@hotmail.com>
2018-11-06 12:41:06 +0100

    albaran_david

---
+++ src/view_miglayout/Albaran.java
@@ -0,0 +1,162 @@
+package view_miglayout;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.ScrollPane;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import config.ConfigurationLoader;
+import idao.ILanguage;
+import model.Client;
+
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Point;
+
+public class Albaran extends JFrame {
+
+	// Atributos globales.
+	private ILanguage language;
+	private ConfigurationLoader configLoad;
+	private Client client;
+	private String username;
+	
+	// Atributos de la clase.
+	private JPanel contentPane;
+	private JTable table, table_1;
+	private JLabel lblAlbaran, lblCliente, lblEmpresa, lblNombreempresa, lblEmpleado, lblNombreempleado,
+	 lblNombre, lblPrimerApellido, lblSegundoApellido, lblDireccion, lblCorreo, lblNombrecliente, lblPriapellido, 
+	 lblSegapellido, lblDireccioncliente, lblCorreocliente;
+	private JScrollPane sp, sp2;
+
+	/**
+	 * Create the frame.
+	 */
+	public Albaran(ConfigurationLoader configLoad, ILanguage language, String username, Client client) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 748, 596);
+		this.contentPane = new JPanel();
+		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		this.contentPane.setLayout(new MigLayout("", "[grow][grow][grow][grow]", "[][75px][50px][][][][][][50px][grow][grow]"));
+		
+		this.lblAlbaran = new JLabel("ALBARAN");
+		this.lblAlbaran.setFont(new Font("Tahoma", Font.BOLD, 17));
+		this.contentPane.add(lblAlbaran, "cell 2 1");
+		
+		this.lblCliente = new JLabel("Cliente:");
+		this.lblCliente.setFont(new Font("Tahoma", Font.BOLD, 15));
+		this.contentPane.add(lblCliente, "cell 0 2,aligny bottom");
+		
+		this.lblEmpresa = new JLabel("Empresa:");
+		this.lblEmpresa.setFont(new Font("Tahoma", Font.BOLD, 15));
+		this.contentPane.add(lblEmpresa, "cell 2 2,aligny bottom");
+		
+		this.lblNombre = new JLabel("Nombre:");
+		this.lblNombre.setFont(new Font("Tahoma", Font.BOLD, 11));
+		this.contentPane.add(lblNombre, "flowx,cell 0 3");
+		
+		this.lblNombreempresa = new JLabel("SEAT");
+		this.lblNombreempresa.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		this.contentPane.add(lblNombreempresa, "cell 2 3");
+		
+		this.lblPrimerApellido = new JLabel("Primer Apellido:");
+		this.lblPrimerApellido.setFont(new Font("Tahoma", Font.BOLD, 11));
+		this.contentPane.add(lblPrimerApellido, "flowx,cell 0 4");
+		
+		this.lblEmpleado = new JLabel("Empleado:");
+		this.lblEmpleado.setFont(new Font("Tahoma", Font.BOLD, 15));
+		this.contentPane.add(lblEmpleado, "cell 2 4,aligny bottom");
+		
+		this.lblSegundoApellido = new JLabel("Segundo Apellido:");
+		this.lblSegundoApellido.setFont(new Font("Tahoma", Font.BOLD, 11));
+		this.contentPane.add(lblSegundoApellido, "flowx,cell 0 5");
+		
+		this.lblNombreempleado = new JLabel("");
+		this.lblNombreempleado.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		this.contentPane.add(lblNombreempleado, "cell 2 5");
+		
+		this.lblDireccion = new JLabel("Direccion:");
+		this.lblDireccion.setFont(new Font("Tahoma", Font.BOLD, 11));
+		this.contentPane.add(lblDireccion, "flowx,cell 0 6");
+		
+		this.lblCorreo = new JLabel("Correo:");
+		this.lblCorreo.setFont(new Font("Tahoma", Font.BOLD, 11));
+		this.contentPane.add(lblCorreo, "flowx,cell 0 7");
+		
+		this.lblNombrecliente = new JLabel("");
+		this.contentPane.add(lblNombrecliente, "cell 0 3");
+		
+		this.lblPriapellido = new JLabel("");
+		this.contentPane.add(lblPriapellido, "cell 0 4");
+		
+		this.lblSegapellido = new JLabel("");
+		this.contentPane.add(lblSegapellido, "cell 0 5");
+		
+		this.lblDireccioncliente = new JLabel("");
+		this.contentPane.add(lblDireccioncliente, "cell 0 6");
+		
+		this.lblCorreocliente = new JLabel("");
+		this.contentPane.add(lblCorreocliente, "cell 0 7");
+		
+		// JTable de las especificaciones seleccionadas anteriormente.
+		this.table = new JTable();
+		this.table.setModel(new DefaultTableModel(
+			// Filas y columnas
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			// Encabezado de las columnas.
+			new String[] {
+				"Cantidad", "Especificacion", "Precio", "Informe"
+			}
+		));
+		this.table.setBorder(new EmptyBorder(0, 0, 0, 0));
+		// Para añadir los Encabezados se necesita crear un JScrollPane y añadirlo al JTable.
+		this.sp = new JScrollPane();
+		this.sp.setViewportBorder(new EmptyBorder(0, 0, 0, 0));
+		this.sp.setBounds(105, 127, 1120, 540);
+		this.sp.setBorder(new EmptyBorder(0, 0, 0, 0));
+		this.sp.setViewportView(table);
+		this.contentPane.add(sp, "cell 0 9 4 1,growx");
+		
+		// Este JTable es para registrar todos los precios.
+		this.table_1 = new JTable();
+		this.table_1.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+			},
+			new String[] {
+				"Precio Base", "Descuento", "Precio Total", "IVA(21%)"
+			}
+		));
+		
+		// Para los Encabezados se crea otro JScrollPane.
+		this.sp2 = new JScrollPane();
+		this.sp2.setViewportBorder(new EmptyBorder(0, 0, 0, 0));
+		this.sp2.setBounds(105, 127, 1120, 540);
+		this.sp2.setBorder(new EmptyBorder(0, 0, 0, 0));
+		this.sp2.setViewportView(table_1);
+		this.table_1.getColumnModel().getColumn(1).setPreferredWidth(102);
+		this.contentPane.add(sp2, "cell 0 10,grow");
+	}
+
+}
---
+++ src/view_miglayout/Selection_Modelo.java
@@ -0,0 +1,277 @@
+package view_miglayout;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+
+import config.ConfigurationLoader;
+import daoImplFactory.LanguageFactory;
+import idao.ILanguage;
+import model.Accessory;
+import model.Client;
+import model.Engine;
+import model.Model;
+import net.miginfocom.swing.MigLayout;
+
+public class Selection_Modelo extends JFrame{
+	
+//	public static void main(String[] args) {
+//		ConfigurationLoader configLoad = ConfigurationLoader.getConfigurationLoaderInstance();
+//		ILanguage language = LanguageFactory.getLanguage(configLoad.getLanguage_default());
+//		part12 p = new part12(configLoad, language, "alexis.mengual");
+//	}
+	
+
+	private ILanguage language;
+	private ConfigurationLoader configLoad;
+	private Client client;
+	private String username;
+	private Model model;
+	private Engine engine;
+	private Accessory accessory;
+	
+//	private JFrame frame;
+	private JPanel panelGBC=new JPanel(),panelBox=new JPanel();
+	private List<JButton> listBotones;
+	private JButton siguiente,anterior,btnCoches;
+	private JLabel l1,luser;
+	private JTextPane areaInfo;
+	private JTextPane area;
+	private ImageIcon coche;
+	private ImageIcon cocheBoton;
+	private JScrollPane scroll;
+	private int posicion=0;
+	
+	String[] imatge_nom;
+	String root_images;
+	List<Image> listImg;
+	
+	/*
+	 * Añado los componentes del concesionaro del coche (parte 12)
+	 */
+	public Selection_Modelo(ConfigurationLoader configLoad, ILanguage language, String username, Client client){
+		this.configLoad = configLoad;
+		this.language = language;
+		this.root_images = this.configLoad.getCar_image_path();
+		this.client = client;
+		this.username = username;
+		this.model = new Model(this.configLoad);
+		this.engine = new Engine(this.configLoad);
+		this.accessory = new Accessory(this.configLoad);
+		//migLayout
+		this.panelGBC.setLayout(new MigLayout("insets 50, fillx, filly"));
+		this.panelBox.setLayout(new BoxLayout(panelBox, BoxLayout.Y_AXIS));
+		this.panelGBC.setBackground(new Color(255, 255, 255));
+		this.panelBox.setBackground(new Color(255, 255, 255));
+		
+		this.l1=new JLabel(variables.Lenguaje.tituloTexto);
+		this.l1.setFont(new java.awt.Font("Tahoma", 0, 16));
+		this.luser=new JLabel(this.language.labelAuthIn() + username);
+		this.luser.setFont(new java.awt.Font("Tahoma", 0, 10));
+		
+		this.area=new JTextPane();
+			area.setContentType("text/html");
+			area.setEditable(false);
+		this.areaInfo=new JTextPane();
+			areaInfo.setContentType("text/html");
+			areaInfo.setEditable(false);
+		this.anterior=new JButton(variables.Lenguaje.bAnterior);
+		this.anterior.setFont(new java.awt.Font("Tahoma", 0, 12));
+		this.anterior.setBackground(new Color(215,18,43));
+		this.anterior.setForeground(new Color(255,255,255));
+		this.anterior.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createLineBorder(new Color(215, 18, 43)),
+				BorderFactory.createEmptyBorder(5,10,5,10)
+				));
+		this.siguiente=new JButton(variables.Lenguaje.bSiguiente);
+		this.siguiente.setFont(new java.awt.Font("Tahoma", 0, 12));
+		this.siguiente.setBackground(new Color(215,18,43));
+		this.siguiente.setForeground(new Color(255,255,255));
+		this.siguiente.setBorder(BorderFactory.createCompoundBorder(
+				BorderFactory.createLineBorder(new Color(215, 18, 43)),
+				BorderFactory.createEmptyBorder(5,10,5,10)
+				));
+		
+												//se añade la ruta de la imagen en una lista
+		this.imatge_nom = this.model.getImage_name();
+		List<String> listImg=new ArrayList<>();
+		for (int i = 0; i < imatge_nom.length; i++) {
+			listImg.add(this.root_images + this.imatge_nom[i]);
+			listImg.add(this.root_images + this.imatge_nom[i]);
+		}
+	
+		//creamos una lista de los botones con las rutas de las imagenes
+		this.listBotones=new ArrayList<JButton>();
+		crearBoton(listImg);//lista ruta imagen
+		
+		//añadimos los botones al panel
+		for (JButton jButton : listBotones) {
+			this.panelBox.add(jButton);
+		}
+
+
+		
+		this.scroll = new JScrollPane(panelBox);
+		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+	    scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+	    scroll.setMinimumSize(new Dimension(coche.getImage().getWidth(null)/3-10, coche.getImage().getHeight(null)*3/2));
+		scroll.setPreferredSize(new Dimension(coche.getImage().getWidth(null)/3-10, coche.getImage().getHeight(null)*3/2));
+		scroll.setBorder(null);
+		
+							//Añadimos las dimensiones aqui porque si hago como el scroll me dice que son 0,0, asi, me lo pilla bien
+		area.setMinimumSize(new Dimension((scroll.getMinimumSize().width+10)*3,scroll.getMinimumSize().height*2/3));
+
+//		listBotones.get(0).doClick();
+		
+		this.panelGBC.add(l1, "span 2");
+		this.panelGBC.add(luser, "wrap, align right");
+		this.panelGBC.add(scroll, "align right");
+		this.panelGBC.add(area, "wrap, pushx, growx, pushy, growy, span 2");
+		this.panelGBC.add(areaInfo, "span 2, skip, wrap, align right, pushx, growx");
+		this.panelGBC.add(anterior, "skip");
+		this.panelGBC.add(siguiente, "align right");
+		
+		
+		area.addComponentListener(new ComponentListener() {
+			
+			@Override
+			public void componentShown(ComponentEvent e) {
+			}
+			
+			@Override
+			public void componentResized(ComponentEvent e) {
+				area.setMargin(new Insets(area.getBounds().height/2-area.getBounds().y, 0, 0, 0));
+			}
+			
+			@Override
+			public void componentMoved(ComponentEvent e) {
+			}
+			
+			@Override
+			public void componentHidden(ComponentEvent e) {
+			}
+		});
+		
+		anterior.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				anteriorActionPerformed();
+			}
+		});
+		
+		siguiente.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				siguienteActionPerformed();
+			}
+		});
+		
+		
+		JFrame();
+	}
+	
+	/*
+	 * Método para configurar la ventana actual.
+	 */
+	private void JFrame() {
+		add(panelGBC);
+		setTitle("SEAT Configurador - Selección de modelo");
+		setIconImage(getIconImage());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(600,600);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	private void crearBoton(List<String> nom) {
+		for (String string : nom) {
+			btnCoches=new JButton();
+			btnCoches.setBackground(new Color(255,255,255));
+			btnCoches.setBorder(BorderFactory.createCompoundBorder(
+					BorderFactory.createLineBorder(new Color(255, 255, 255)),
+					BorderFactory.createEmptyBorder(1, 1, 1, 1)
+					));
+			coche=new ImageIcon(string);
+			cocheBoton= new ImageIcon(coche.getImage().getScaledInstance(coche.getIconWidth()/4, coche.getIconHeight()/4, Image.SCALE_DEFAULT));
+			btnCoches.setIcon(cocheBoton);
+			btnCoches.setMargin(new Insets(0, 0, 0, 0));
+			listBotones.add(btnCoches);
+			
+			listImg=new ArrayList<Image>();
+			listImg.add(coche.getImage());
+			
+			listener(coche, btnCoches);  
+		}
+		
+		
+	}
+	private void listener(ImageIcon coch, JButton b) {
+		b.addFocusListener(new FocusListener() {
+			
+			@Override
+			public void focusLost(FocusEvent e) {
+				b.setBackground(new Color(255,255,255));
+				b.setBorder(BorderFactory.createCompoundBorder(
+						BorderFactory.createLineBorder(new Color(255, 255, 255)),
+						BorderFactory.createEmptyBorder(1, 1, 1, 1)
+						));
+			}
+			
+			@Override
+			public void focusGained(FocusEvent e) {
+				b.setBorder(BorderFactory.createCompoundBorder(
+						BorderFactory.createLineBorder(new Color(215, 18, 43)),
+						BorderFactory.createEmptyBorder(1, 1, 1, 1)
+						));
+				for (int i = 0; i < imatge_nom.length; i++) {
+					if ((root_images+imatge_nom[i]).equals(coch.getDescription())) {//la descripcion es del objeto, que devuelve la ruta de la imagen
+						posicion=i;
+					}
+				}
+				File fImg = new File(coch.getDescription());
+				area.setText("<html><div style='text-align: center;'><span style='background: rgb(215,18,43); color: rgb(255,255,255); padding:10px; font-family: Tahoma;'>"+model.getName()[posicion]+"<br><img src =\""+fImg.toURI()+"\" /></span></div></html>");
+				areaInfo.setText("<html><div style='text-align: center; font-family: Tahoma;'><span style=padding:10px;'>"+model.getDescription()[posicion]+"</span></div><br></html>");
+				System.out.println("[INFO] - Cambiando modelo a: "+model.getName()[posicion]);
+			}
+		});
+	}
+	
+	public Image getIconImage() {
+		File image = new File("src/config/favicon.png");
+        Image retValue = Toolkit.getDefaultToolkit().getImage(image.getAbsolutePath());
+        return retValue;
+    }
+
+	protected void anteriorActionPerformed() {
+		setVisible(false);
+		new Data_Clients(configLoad, language, username, client);
+	}
+	
+	protected void siguienteActionPerformed() {
+		
+	}
+}
